1d646c86112e0a2024c8ee05c92abf5a3bd0d779
A.A <abelsoftware123@hotmail.com>
2026-08-07 01:18:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -258,7 +258,7 @@ async function startApiScan() {
         const url = `${baseUrl}${endpoints[i]}`;
         
         try {
-            const response = await fetchWithTimeout(url, { timeout: 10000 });
+            const response = await fetchWithTimeout(url, { timeout: 20000 });
             
             if (response.status === 200) {
                 vulnerabilities.push({
